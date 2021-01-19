dca8442be3c7cc9fea8efd3a2ee5e1eff0b7be10
Farry-Rangian <farry12angian@gmail.com>
2021-01-19 09:41:16 +0800

    Appending Variables to Strings

--- JavaScript-Algorithms-and-Data-Structures-Certification/1-Basic-JavaScript/app.js
+++ JavaScript-Algorithms-and-Data-Structures-Certification/1-Basic-JavaScript/app.js
@@ -1,3 +1,5 @@
-// Only change code below this line
-var myName = "Farry";
-var myStr = "My name is " + myName + "and I am well";
+// Change code below this line
+
+var someAdjective = "tes";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
